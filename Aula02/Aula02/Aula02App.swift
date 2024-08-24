4a4e5ebff9bbe90b866e36dd963948e182ef896b
Alan Gabriel <115121549+Alan-G-S-Oliveira@users.noreply.github.com>
2024-08-23 21:20:44 -0300

    Aula 3

---
+++ Aula02/Aula02/Aula02App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula02App.swift
+//  Aula02
+//
+//  Created by Turma02-1 on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Aula02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aula02/Aula02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aula02
+//
+//  Created by Turma02-1 on 23/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
